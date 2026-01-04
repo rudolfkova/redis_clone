a43d373390e7f4b83cd8eb6903d81e1fd38abc3e
Smirnov Nikita <smirnov_niki@bk.ru>
2026-01-04 15:23:26 +0300

    Shards

--- internal/storage.go
+++ internal/storage.go
@@ -1,37 +1,69 @@
 package redis
 
 import (
+	"hash/fnv"
 	"sync"
 )
 
-type Redis struct {
+const (
+	shardsNum = 64
+)
+
+type Shard struct {
 	storage map[string]string
 	mu      sync.RWMutex
 }
 
+type Redis struct {
+	shards []*Shard
+}
+
 func NewStorage() *Redis {
-	return &Redis{
-		storage: make(map[string]string),
+	r := &Redis{
+		shards: make([]*Shard, shardsNum),
 	}
+
+	for s := range r.shards {
+		r.shards[s] = &Shard{}
+		r.shards[s].makeMap()
+	}
+
+	return r
 }
 
-func (s *Redis) Set(key string, value string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.storage[key] = value
+func (s *Shard) makeMap() {
+	s.storage = make(map[string]string)
+}
+
+func (r *Redis) Set(key string, value string) {
+	hash := hashKey(key) % shardsNum
+
+	r.shards[hash].mu.Lock()
+	r.shards[hash].storage[key] = value
+	r.shards[hash].mu.Unlock()
 
 }
 
-func (s *Redis) Get(key string) (string, bool) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	v, ok := s.storage[key]
+func (r *Redis) Get(key string) (string, bool) {
+	hash := hashKey(key) % shardsNum
+
+	r.shards[hash].mu.RLock()
+	v, ok := r.shards[hash].storage[key]
+	r.shards[hash].mu.RUnlock()
 
 	return v, ok
 }
 
-func (s *Redis) Delete(key string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	delete(s.storage, key)
+func (r *Redis) Delete(key string) {
+	hash := hashKey(key) % shardsNum
+
+	r.shards[hash].mu.Lock()
+	delete(r.shards[hash].storage, key)
+	r.shards[hash].mu.Unlock()
+}
+
+func hashKey(key string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return h.Sum32()
 }
